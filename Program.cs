fb6c630962ca55182f9f6387b5cb9825ac29aa60
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-09-20 16:14:07 -0500

    Added a function that gets called inside of the main function and conditional statement that retturns greeting based on the data passed to the function

--- Program.cs
+++ Program.cs
@@ -7,6 +7,24 @@ namespace helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            myFunction();
+            conditionals("Hamza");
+        }
+
+        static void myFunction() {
+            Console.WriteLine("Hello Every one?");
+
+        }
+
+        static void conditionals(string firstName) {
+
+            if(firstName == "Hamza"){
+                Console.WriteLine("Hi" + " " + firstName);
+            }else {
+                Console.WriteLine("Hello" + " " + "stranger");
+            }
+
+
         }
     }
 }
