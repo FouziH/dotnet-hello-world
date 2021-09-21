ae5f34990c2608dfc610856468ac899390de57ac
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-09-20 20:25:31 -0500

    Answered the love functtion kata

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace helloworld
             Console.WriteLine("Hello World!");
             myFunction();
             conditionals("Hamza");
+            lovefunc(2, 2);
         }
 
         static void myFunction() {
@@ -20,11 +21,26 @@ namespace helloworld
 
             if(firstName == "Hamza"){
                 Console.WriteLine("Hi" + " " + firstName);
+                
             }else {
                 Console.WriteLine("Hello" + " " + "stranger");
             }
 
+        }
+          public static bool lovefunc(int flower1, int flower2) { 
+         if (flower1 % 2 == 0 && flower2 % 2 == 0 || flower1 % 2 != 0 && flower2 % 2 != 0) {
+
+                return false;
+        }  
+
+        else 
+
+        {
+                return true;
 
         }
     }
+    }
 }
+
+
